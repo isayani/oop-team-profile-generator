e952fa7020a41edb0e0119820dd6579dac7c9ffc
isayani <isayani95@gmail.com>
2022-09-14 23:29:35 -0400

    completed inquirer prompts

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generateHTML = require('./src/generateHTML');
+
+let allResponses = {
+    manager: [],
+    employee: [],
+    engineer: [],
+    intern: [],
+};
     
-function managerPrompt() {
+const mgr = function managerPrompt() {
     inquirer
     .prompt([
         {
@@ -28,11 +35,136 @@ function managerPrompt() {
         {
             type: 'checkbox',
             message: 'Would you like to add anymore employees?',
-            choices: [  "Engineer", new inquirer.Separator(), "Intern", new inquirer.Separator(), "No, I am finished building my team."],
+            choices: ["Engineer", new inquirer.Separator(), "Intern", new inquirer.Separator(), "No, I am finished building my team."],
             name: 'option',
         },
     ])
+
+    // add in conditional for engineer, intern or none
     .then ((input) => {
-        
+        allResponses.manager.push({...input});
+
+        if (mgr.input.choices === "Engineer") {
+            engineerPrompt();
+        } else if (mgr.input.choice === "Intern") {
+            internPrompt();
+        } else {
+            generateHTML(allResponses);
+        }
+    })
+
+    .catch((err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(
+            "Success! Manager added to roster.",
+          );
+        }
     });
-}
+};
+
+const eng = function engineerPrompt() {
+    inquirer
+    .prompt([
+        {
+            message: "What is the team engineer's name?",
+            name: 'engName',
+        },
+        {
+            type: 'number',
+            message: "What is the team engineer's employee ID?",
+            name: 'engId',
+        },
+        {
+            message: "Please enter the engineer's email address:",
+            name: 'engEmail',
+        },
+        {
+            message: "What is the engineer's GitHub username?",
+            name: 'gitHub',
+        },
+        
+        {
+            type: 'checkbox',
+            message: 'Would you like to add anymore employees?',
+            choices: ["Intern", new inquirer.Separator(), "No, I am finished building my team."],
+            name: 'option',
+        },
+    ])
+    // add in conditional for engineer, intern or none
+    .then ((input) => {
+        allResponses.engineer.push({...input});
+
+        if (eng.input.choices === "Intern") {
+            internPrompt();
+        } else {
+            generateHTML(allResponses)
+        }
+    })
+
+    .catch((err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(
+            "Success! Engineer added to roster.",
+          );
+        }
+      });
+};
+
+const int = function internPrompt() {
+    inquirer
+    .prompt([
+        {
+            message: "What is the intern's name?",
+            name: 'intName',
+        },
+        {
+            type: 'number',
+            message: "What is the intern's employee ID?",
+            name: 'intId',
+        },
+        {
+            message: "Please enter the intern's email address:",
+            name: 'intEmail',
+        },
+        {
+            message: "What is the interns's school?",
+            name: 'school',
+        },
+        {
+            type: 'checkbox',
+            message: 'Would you like to add anymore employees?',
+            choices: ["Engineer", new inquirer.Separator(), "Intern", new inquirer.Separator(), "No, I am finished building my team."],
+            name: 'option',
+        },
+        
+    ])
+    
+    .then ((input) => {
+        allResponses.intern.push({...input});
+        console.log("Intern added to roster!");
+
+        if (mgr.input.choices === "Engineer") {
+            engineerPrompt();
+        } else if (mgr.input.choice === "Intern") {
+            internPrompt();
+        } else {
+            generateHTML(allResponses);
+        }
+    })
+
+    .catch((err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(
+            "Rendering your Team Profile!",
+          );
+        }
+      });
+};
+
+managerPrompt();
