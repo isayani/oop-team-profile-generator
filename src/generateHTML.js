a21a36d89a79399df4f9c5ea7464f96233269588
isayani <isayani95@gmail.com>
2022-09-19 23:07:38 -0400

    generate Cards and HTML functions

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,6 +1,53 @@
-// needs to be broken up by first, middle, last
-const generateHTML = ({}) => 
-`<!DOCTYPE html>
+// Function to auto generate cards per employee
+const generateCards = (team) => {
+  let html = ''
+
+  for (let i = 0; i < team.length; i++) {
+
+    if (team[i].getRole() == 'Manager') {
+      html += `
+      <div class="container card border-secondary bg-light my-5 ">
+      ${team[i].getName()} <br/>
+      Manager <br/>
+      <br/>
+      ID: ${team[i].getId()} <br/>
+      Email: ${team[i].getEmail()} <br/>
+      Office Number: ${team[i].getOfficeNumber()}
+      </div>
+      `
+    } else if (team[i].getRole() == 'Engineer') {
+      html += `
+      <div class="container card border-secondary bg-light my-5 ">
+      ${team[i].getName()} <br/>
+      Engineer <br/>
+      <br/>
+      ID: ${team[i].getId()} <br/>
+      Email: ${team[i].getEmail()} <br/>
+      GitHub: https://github.com/${team[i].getGithub()}
+      </div>
+      `
+    } else {
+      html += `
+      <div class="container card border-secondary bg-light my-5 ">
+      ${team[i].getName()} <br/>
+      Intern <br/>
+      <br/>
+      ID: ${team[i].getId()} <br/>
+      Email: ${team[i].getEmail()} <br/>
+      School: ${team[i].getSchool()}
+      </div>
+      `
+    }
+    
+  }
+  return html;
+}
+
+// GenerateHTML function has template literals for index.html and calls generateCards function to main
+const generateHTML = (team) => 
+{
+
+ return `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -11,52 +58,30 @@ const generateHTML = ({}) =>
 <body>
   <header class="jumbotron jumbotron-fluid text-center">
   <div class="container">
-    <h1 class="display-4">Your ${title}'s README is ready!</h1>
-    <p class="lead">Please copy it from below:</p>
+    <h1 class="display-4">Your Team Roster is ready!</h1>
   </div>
 </header>
 
 <main>
-    <div class="container card border-secondary bg-light my-5 ">
-    <h2>${title}</h2>
-    <br/>
-    <h3>Description</h3>
-    <p>${descr}</p>
-    <br/>
-    <h3>User Story</h3>
-    <p>${userStory}</p>
-    <br/>
-    <h3>Acceptance Criteria</h3>
-    <p>${accCriteria}</p>
-    <br/>
-    <h3>Technologies Employed</h3>
-    <p>${tech}</p>
-    <br/>
-    <h3>Future Development</h3>
-    <p>${futureDev}</p>
-    <br/>
-    <h3>Installation and Mock-Up</h3>
-    <p>${install}</p>
-    <p>You can see the deployed application at ${deplLink}</p>
-    <p>${mock}</p>
-    <br/>
-    <h3>Issues</h3>
-    <p>To report any issues, visit ${issues}</p>
-    <br/>
-    <h3>Contributions</h3>
-    <p>To contribute to this application, visit ${repo} </p>
-    </div>
+
+${generateCards(team)}
+
 </main>
 
 <footer class="jumbotron jumbotron-fluid">
     <div class="container text-center">
-    <h4 class="">This generator was made possible by <a href="https://github.com/isayani/pro-readme-generator"class="badge badge-secondary">isayani</a> and</h4>
-    <ul class="list-group w-50 mx-auto">
-      <li class="list-group-item">Node.js</li>
-      <li class="list-group-item">Inquirer.js</li>
+    <h4>This generator was made possible by <a href="https://github.com/isayani/"class="badge badge-secondary">isayani</a> and</h4>
+    <ul class="list-group w-50 mx-auto text-decoration-none">
+      <a href="https://nodejs.org/en/docs/" class="list-group-item text-secondary">Node.js</a>
+      <a href="https://www.npmjs.com/package/inquirer/v/8.2.4" class="list-group-item text-secondary">Inquirer.js</a>
+      <a href="https://jestjs.io/" class="list-group-item text-secondary">Jest.js</a>
     </ul>
     </div>
 </footer>
 </body>
 </html>
-`;
+`
+};
+
+// Exports file to use in index.js
+module.exports = generateHTML;
